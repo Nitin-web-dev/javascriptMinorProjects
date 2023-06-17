435bedefeae5dc47433195a715f796cc5f09d42b
Nitin Singh <Nitinwebdev5@gmail.com>
2023-06-17 10:49:02 +0530

    added some icon and did api update

--- WheatherApp/index.js
+++ WheatherApp/index.js
@@ -4,6 +4,7 @@ const dateelement = document.querySelector('#dttext');
 const toggleimg = document.querySelector("#modeImg");
 const details = document.querySelector("#dtail");
 const tmp = document.querySelector('#Temprature');
+const iconsWeather = document.querySelector("#icon");
 
 toggleModebtn.addEventListener("click", () => {
   if (!toggleModebtn.classList.contains("nightbtn")) {
@@ -41,29 +42,42 @@ let todayDate = datearr[date.getMonth()] + " " + date.getDate();
 
 dateelement.innerText = todayDate;
 
+document.body.onload = () => {
+ getWeatherData();
+}
+
+const getWeatherData = async () => {
     // api key for wheather api
     apik = '3045dd712ffe6e702e3245525ac7fa38';
     apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${'Delhi'}&appid=${apik}`;
    
-    fetch(apiUrl) /// fetch api data will come in json
-    .then(res => res.json()) // this will convert response data to json object
-    .then(data => {
+
+       let data = await fetch(apiUrl) /// fetch api data will come in json
+      let res = await data.json() // this will convert response data to json object
+      let data2= [res];
+      console.log(data2);
+   
 
         // collect all imortand data from fetch data;
-        let nameval = data.name;
-        let desc = data.weather[0].description;
-        let temperature = data.main.temp;
-        let wndspd = data.wind.speed;
+        let nameval = data2[0].name;
+        let desc = data2[0].weather[0].main;
+        let temperature = data2[0].main.temp;
+        // let wndspd = data2.wind.speed;
 
-          // console.log(nameval, desc, temperature, wndspd);
+          console.log(nameval,desc,temperature);
 
 
           details.innerHTML = `${nameval} weather: ${desc}`; 
           tmp.innerHTML = `${conver(temperature)} C`
-        // we will get temp we have to fixed the value;
+        // // we will get temp we have to fixed the value;
         function conver(val) {
-            return (val - 273).toFixed(2);
+            return (val - 273).toFixed(1);
         }
 
 
-      });
+
+      
+      }
+
+
+   
